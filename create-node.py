e9ddf2191154723f474a4d9ea563902cf49bb697
Kevin <walchko@users.noreply.github.com>
2019-06-29 17:03:48 -0400

    Create create-node.py

---
+++ create-node.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+##############################################
+# The MIT License (MIT)
+# Copyright (c) 2019 Kevin Walchko
+# see LICENSE for full details
+##############################################
+
+from pygecko.multiprocessing import geckopy
+
+def main():
+
+
+if __name__ == '__main__':
